d257e62954e48899fa96c68f0bb50393c008edc3
danghieuliem <hieuliem33@gmail.com>
2020-10-15 10:38:54 +0700

    xong roi a' :3

--- weather.js
+++ weather.js
@@ -1,12 +1,14 @@
 const request = require("request");
 const chalk = require("chalk");
 const yargs = require('yargs');
+const moment = require('moment');
 //const readline = require("readline-sync");
 //const { argv, title } = require('process');
 //const { string } = require('yargs');
 const { time } = require("console");
 const { join } = require("path");
 const { command } = require("yargs");
+const { isUndefined } = require("util");
 
 yargs.version('1.1.0')
 
@@ -52,60 +54,55 @@ getLocationWeather = (woeid) => {
                     });
                 }
             }
+            console.log(chalk.greenBright("DONE !"));
         }
     );
 };
-getLocationWeatherAtDate = (woeid, date) => {
-    var date_regex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/;
-    if (!(date_regex.test(testDate))) {
-        return false;
-    }
+getLocationWeatherAtDate = (woeid, date, location) => {
     request(
-        `https://www.metaweather.com/api/location/${woeid}/`,
+        `https://www.metaweather.com/api/location/${woeid}/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}/`,
         { json: true },
         (error, res, body) => {
             if (error === null) {
-                let weathers = body.consolidated_weather;
-                console.log(body.consolidated_weather[0]);
-                if (weathers.length !== 0) {
+                if (body.length !== 0) {
+                    let element = body[body.length - 1];
                     console.log(chalk.greenBright("==================================="));
-                    console.log(chalk.green("Location  : ") + chalk.yellow(body.title));
-                    weathers.forEach((element) => {
-                        console.log(
-                            chalk.greenBright("===================================")
-                        );
-                        console.log(
-                            chalk.green("Date          : ") + element.applicable_date
-                        );
-                        console.log(
-                            chalk.green("Weather       : ") + element.weather_state_name
-                        );
-                        console.log(
-                            chalk.green("Min temp      : ") +
-                            Math.round(element.min_temp * 100) / 100
-                        );
-                        console.log(
-                            chalk.green("Max temp      : ") +
-                            Math.round(element.max_temp * 100) / 100
-                        );
-                        console.log(
-                            chalk.green("Visibility    : ") +
-                            Math.round(element.visibility * 100) / 100
-                        );
-                        console.log(
-                            chalk.green("Air pressure  : ") + element.air_pressure + " Pa"
-                        );
-                        console.log(
-                            chalk.green("Humidity      : ") + element.humidity + "%"
-                        );
-                    });
+                    console.log(chalk.green("Location  : ") + chalk.yellow(location));
+                    console.log(
+                        chalk.greenBright("===================================")
+                    );
+                    console.log(
+                        chalk.green("Date          : ") + element.applicable_date
+                    );
+                    console.log(
+                        chalk.green("Weather       : ") + element.weather_state_name
+                    );
+                    console.log(
+                        chalk.green("Min temp      : ") +
+                        Math.round(element.min_temp * 100) / 100
+                    );
+                    console.log(
+                        chalk.green("Max temp      : ") +
+                        Math.round(element.max_temp * 100) / 100
+                    );
+                    console.log(
+                        chalk.green("Visibility    : ") +
+                        Math.round(element.visibility * 100) / 100
+                    );
+                    console.log(
+                        chalk.green("Air pressure  : ") + element.air_pressure + " Pa"
+                    );
+                    console.log(
+                        chalk.green("Humidity      : ") + element.humidity + "%"
+                    );
                 }
             }
+            console.log(chalk.greenBright("DONE !"));
         }
     );
 };
-
-searchLocation = (key, date = "") => {
+searchLocation = (key, date) => {
+    console.log(chalk.yellow("search Location ...."));
     request(
         `https://www.metaweather.com/api/location/search/?query=${key}`,
         { json: true },
@@ -113,10 +110,12 @@ searchLocation = (key, date = "") => {
             if (body.length === 0) {
                 console.log(chalk.red("NOT FOUND"));
             } else if (body.length === 1) {
-                if (date === "") {
+                console.log(chalk.greenBright("Was found location !"));
+                console.log(chalk.yellow("Geting location weather ....."));
+                if (date === null) {
                     getLocationWeather(body[0].woeid);
                 } else {
-                    getLocationWeatherAtDate(body[0].woeid, date);
+                    getLocationWeatherAtDate(body[0].woeid, date,body[0].title);
                 }
             } else if (body.length > 1) {
                 const limit = 5;
@@ -127,35 +126,39 @@ searchLocation = (key, date = "") => {
                             return item.title;
                         })
                         .join(", ");
-                    secondText = ` and ${body.length()} more ... `;
-                    console.log(fistText + secondText);
+                    secondText = ` and ${body.length} more ... \nPlease enter location correctly!`;
+                    console.log("Was found : "+ chalk.redBright(fistText + secondText));
                 }
             }
         }
     );
 };
-
 yargs.command({
     command: "weather",
     describe: "search location",
     builder: {
         location: {
-            decsribe: "location",
+            describe: "location",
             demandOption: true,
             type: "string",
         },
         date: {
-            decsribe: "Date",
+            describe: "Date",
             demandOption: false,
             type: "string",
         },
     },
     handler: (argv) => {
-        searchLocation(argv.location);
+        let strdate = argv.date;
+        if (isUndefined(strdate)) {
+            searchLocation(argv.location, null);
+        }
+        else if (moment(strdate, "DD/MM/YYYY", true).isValid() === true) {
+            let date = new Date(`${strdate.slice(6, 10)}/${strdate.slice(3, 5)}/${strdate.slice(0, 3)}`);
+            searchLocation(argv.location, date);
+        } else {
+            console.log(chalk.red("Date is not invalid. Date format is DD/MM/YYYY"));
+        }
     },
 });
-
-console.log(yargs.argv);
-
-var date = new Date("2000-11-20");
-console.log(date);
+yargs.argv;
